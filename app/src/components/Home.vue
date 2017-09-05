bc81b421dabddfafcaa2dd2a10c0cd80210d6dbd
N Kort <nics@limnerstudio.co.uk>
2017-09-05 17:54:13 -0400

    clover route

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -31,7 +31,6 @@
 </template>
 
 <script>
-  import Clover from '../assets/clovers'
   import { mapGetters } from 'vuex'
   import SvgText from '@/components/TextPath'
 
@@ -40,8 +39,7 @@
     data () {
       return {
         heart: 'C4E3F4C5D6F3D3C2D2E6F5E7C6B6F7B4E8F8G8G5F6B3D8G3G4H5H6H7F2E1B5D7G6A4C8D1C3E2C1B1G1G2H1G7H8C7F1H2H4B2H3A1A2B8B7A8A7A6A5A3',
-        newName: null,
-        clover: new Clover(true)
+        newName: null
       }
     },
     computed: {
@@ -50,10 +48,11 @@
         return this.clover.byteBoardToRowArray()
       },
 
-      ...mapGetters({
-        balance: 'balance',
-        allClovers: 'allClovers'
-      })
+      ...mapGetters([
+        'balance',
+        'allClovers',
+        'clover'
+      ])
     },
     components: { SvgText }
   }
