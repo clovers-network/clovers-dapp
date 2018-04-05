878aaf9ac4e5932f9dee06be8727d01ae6203690
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-04-04 20:04:20 -0400

    try again

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  lintOnSave: false,
-  configureWebpack: () => {
-     return {
-      "resolve": {
-        "alias": {
-          // given that basscss has been installed with npm
-          "basscss-base": "../node_modules/basscss-base",
-          "basscss-utilities": "../node_modules/basscss-utilities",
-          "basscss-positions": "../node_modules/basscss-positions"
-          // and so on...
-        }
-      },
-    }
-  }
+  lintOnSave: false
 }
