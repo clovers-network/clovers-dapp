037271e20bd7e1dca18fbd2ce3140e5b5d396c85
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-03 20:10:31 +0200

    welcome polish

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -25,7 +25,7 @@
 import Reversi from 'clovers-reversi'
 export default {
   name: 'Welcome',
-  data() {
+  data () {
     return {
       heart:
         'C4E3F4C5D6F3D3C2D2E6F5E7C6B6F7B4E8F8G8G5F6B3D8G3G4H5H6H7F2E1B5D7G6A4C8D1C3E2C1B1G1G2H1G7H8C7F1H2H4B2H3A1A2B8B7A8A7A6A5A3',
@@ -35,23 +35,23 @@ export default {
       random4: null
     }
   },
-  mounted() {
+  mounted () {
     this.$nextTick(() => {
       this.scrollTo()
     })
   },
   methods: {
-    cloverClass(c) {
+    cloverClass (c) {
       return {
-        'no-border': c == 3,
-        'bg-green': c == 3
+        'no-border': c === 3,
+        'bg-green': c === 3
       }
     },
-    scrollTo() {
+    scrollTo () {
       this.$refs.wide.scrollLeft =
         this.$refs.wide.scrollWidth / 2 - window.innerWidth / 2 + 6
     },
-    random() {
+    random () {
       const reversi = new Reversi()
       reversi.mine()
       return reversi.movesString
