44a94064a30ae3acea4529a8b37234eada48fa7e
Alan Woo <public@alanwoo.ca>
2018-08-04 22:06:13 +0200

    Hide menu when clicking links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -21,7 +21,7 @@
       v-show="showMenu"
       class="absolute z1 h-100vh col-12 bg-green top-0 left-0 flex flex-column justify-between center">
       <div class="h-header"/>
-      <nav class="flex-auto flex items-center justify-center">
+      <nav class="flex-auto flex items-center justify-center" @click="showMenu = !showMenu">
         <ul class="h1 list-reset">
           <li><router-link :to="{ name: 'Welcome' }">About</router-link></li>
           <li class="mt2"><router-link :to="{ name: 'Field' }">Find Clovers</router-link></li>
