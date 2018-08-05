75a9bef2945afb1ffefb71f6098732f6ed9bb4b5
evvvritt <evv.info@gmail.com>
2018-08-05 17:28:16 +0200

    Field - sm-col-3, item uses square frame with .pb-100

--- src/views/Field.vue
+++ src/views/Field.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="mt3 mb-full-height">
+  <div class="mt2 mb-full-height">
     <single-view v-show="viewSingle" :clover="viewSingle" @close="viewSingle = null"></single-view>
-    <ul class="list-reset flex flex-wrap mxn2 mt0 mb3 px1">
-      <li v-for="(clover, i) in generated" :key="i" class="px2 mb3 col-6 relative appear-off" data-expand="-50" :data-appear="i % 3">
-        <img :src="cloverImage(clover)" @click="viewSingle = clover" class="block mx-auto pointer"/>
+    <ul class="list-reset flex flex-wrap mxn2 mt0 mb3 px2">
+      <!-- item -->
+      <li v-for="(clover, i) in generated" :key="i" class="p2 col-6 sm-col-4 relative appear-off" data-expand="-50" :data-appear="i % 3">
+        <div class="pb-100 relative">
+          <div class="absolute overlay flex">
+            <!-- image -->
+            <img :src="cloverImage(clover)" @click="viewSingle = clover" class="block m-auto pointer"/>
+          </div>
+        </div>
+        <!-- fav btn -->
         <div class="h2 line-height-1 absolute top-0 right-0 mr2">
           <span v-if="isSaved(clover)" class="green">&hearts;</span>
           <a v-else @click="saveClover(clover)" class="green pointer" style="opacity:.3">&hearts;</a>
@@ -97,34 +104,24 @@ export default {
 
 <style>
   .mb-full-height {
-    margin-bottom: calc(200vh - 275px);
+    margin-bottom: calc(150vh - 275px);
   }
 
-  .appear {
-    animation-name: appear;
-    animation-duration: 600ms;
-  }
-
-  @keyframes appear {
-    from { opacity: 0; }
-    to { opacity: 1; }
-  }
-
-  [data-appear] > img{ animation-duration: 800ms; }
-  [data-appear="0"] > img{ animation-name: appear0; }
-  [data-appear="1"] > img{ animation-name: appear1; }
-  [data-appear="2"] > img{ animation-name: appear2; }
+  [data-appear] img{ animation-duration: 800ms; }
+  [data-appear="0"] img{ animation-name: appear0; }
+  [data-appear="1"] img{ animation-name: appear1; }
+  [data-appear="2"] img{ animation-name: appear2; }
 
   @keyframes appear0 {
-    from { opacity: 0; transform: translate(10%, 5%);}
+    from { opacity: 0; transform: translate(8%, 3%);}
     to { opacity: 1; transform: none;}
   }
   @keyframes appear1 {
-    from { opacity: 0; transform: translate(-5%, -10%); }
+    from { opacity: 0; transform: translate(-3%, 8%); }
     to { opacity: 1; }
   }
   @keyframes appear2 {
-    from { opacity: 0; transform: translate(-10%, 5%);}
+    from { opacity: 0; transform: translate(-8%, 3%);}
     to { opacity: 1; transform: none;}
   }
 </style>
