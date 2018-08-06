9deb354352106764ee8b951d3ee5ff18b169e441
evvvritt <evv.info@gmail.com>
2018-08-06 19:24:38 +0200

    AppHeader / WaveyMenu.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,8 +14,8 @@
           class="menu-btn pointer"
           @click="showMenu = !showMenu"
           aria-label="Toggle Menu">
-            <div v-if="mining" id="wave" :class="{ whiteWave : showMenu }"></div>
-            <img v-else :src="showMenu
+            <wavey-btn v-show="mining" :isWhite="showMenu"></wavey-btn>
+            <img class="block" v-show="!mining" :src="showMenu
               ? require('../assets/icons/hamburger-white.svg')
               : require('../assets/icons/hamburger.svg')" />
         </button>
@@ -63,6 +63,7 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex'
+import WaveyBtn from '@/components/Icons/WaveyMenu'
 import Pig from '@/components/Pig'
 import PersonIcon from '@/components/Icons/PersonIcon'
 export default {
@@ -94,41 +95,8 @@ export default {
       }
     }
   },
-  components: { Pig, PersonIcon }
+  components: { Pig, PersonIcon, WaveyBtn }
 }
 </script>
 
-<style>
-@keyframes wave {
-  0% {
-    background-position: 0 0;
-  }
-  100% {
-    background-position: 48px 0;
-  }
-}
-
-div#wave {
-  width:48px;
-  position:relative;
-  height: 14px;
-  background: url('../assets/icons/wave.svg') repeat-x 0%;
-  background-size: contain;
-  animation: 3s wave linear infinite;
-  box-shadow:inner 0 0 1rem rgba(red,1);
-
-  &.whiteWave {
-    background: url('../assets/icons/wave-white.svg') repeat-x 0%;
-  }
-
-  &::before{
-    content:"";
-    background:linear-gradient(90deg, rgba(white,0) 25%, rgba(white,1) 75%);
-    height:100%;
-    width:100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-}
-</style>
+<style></style>
---
+++ src/components/Icons/WaveyMenu.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  div(:class="{'wave-btn--white': isWhite}")
+</template>
+
+<script>
+export default {
+  name: 'WaveyMenu',
+  props: {
+    isWhite: {type: Boolean, default: false}
+  }
+}
+</script>
+
+<style scoped>
+div{
+  width:48px;
+  position:relative;
+  height: 14px;
+  background: url('../../assets/icons/wave.svg') repeat-x 0%;
+  background-size: contain;
+  animation: 3s wave linear infinite;
+  box-shadow:inner 0 0 1rem rgba(red,1);
+
+  &.wave-btn--white {
+    background: url('../../assets/icons/wave-white.svg') repeat-x 0%;
+  }
+
+  &::before{
+    content:"";
+    background:linear-gradient(90deg, rgba(white,0) 25%, rgba(white,1) 75%);
+    height:100%;
+    width:100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+@keyframes wave {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 48px 0;
+  }
+}
+</style>
