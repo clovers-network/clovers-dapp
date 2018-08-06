88eddce26c2308f334c14bdaa5bad68cb0326987
Alan Woo <public@alanwoo.ca>
2018-08-06 22:39:27 +0200

    Rename menu/account to menu/my clovers

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -54,7 +54,7 @@
           <li><router-link :to="{ name: 'Welcome' }">About</router-link></li>
           <li class="mt2"><router-link :to="{ name: 'Field' }">Find Clovers</router-link></li>
           <li class="mt2"><router-link :to="{ name: 'Feed' }">Feed</router-link></li>
-          <li class="mt2"><router-link :to="{ name: 'Account' }">Account</router-link></li>
+          <li class="mt2"><router-link :to="{ name: 'Account' }">My Clovers</router-link></li>
 
         </ul>
       </nav>
