f0090ee047d9ac0d43aa1bb4663c7d760810fcd0
Alan Woo <public@alanwoo.ca>
2018-08-07 17:23:52 +0200

    Init webpack analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
+  	plugins: [new BundleAnalyzerPlugin()],
     output: {
       globalObject: 'this'
     }
