421f07bb0903c910fe77ac389137157c7c72b9f8
Alan Woo <public@alanwoo.ca>
2018-08-07 17:29:32 +0200

    Remove moment locales, add worker

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const webpack = require('webpack')
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
-  	plugins: [new BundleAnalyzerPlugin()],
+  	plugins: [
+  		new BundleAnalyzerPlugin(),
+  		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  	],
     output: {
       globalObject: 'this'
     }
