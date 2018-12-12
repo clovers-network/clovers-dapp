c6567a1b4a4f5274c0e3cc3b9e77154b61d434f4
evvvritt <evv.info@gmail.com>
2018-12-12 14:37:06 +0100

    router - use archived file for cloverList (/ why do we need?)

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ import MyClovers from '@/views/MyClovers'
 // import Clover from '@/components/Clover'
 // import User from '@/components/User'
 // import Users from '@/components/Users'
-import CloverList from '@/components/CloverList'
+import CloverList from '@/components/archive/0.1.0/CloverList'
 
 Vue.use(Router)
 
