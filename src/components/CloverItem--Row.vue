1b9333f94c7f466f4211aa593858f6ebe1f68625
evvvritt <evv.info@gmail.com>
2018-12-18 17:22:40 +0100

    MyClovers - use CloversItem--Row.vue for list items

--- src/components/CloverItem--Row.vue
+++ src/components/CloverItem--Row.vue
@@ -6,14 +6,14 @@
     <div class="col-3 center">
       <img :src="cloverImage(clover, 64)" width="64" height="64"/>
     </div>
-
-    <div class="col-3 flex flex-column justify-center">
+    <!-- name -->
+    <div class="flex flex-column justify-center" :class="{'col-3': showOwnerCol, 'col-6': !showOwnerCol}">
       <h3 class="h4 truncate font-mono">
         <span v-if="clover.name !== clover.board">{{ clover.name }}</span>
       </h3>
     </div>
-
-    <div class="col-3 flex flex-column justify-center px2">
+    <!-- owner / RFT market cap -->
+    <div class="col-3 flex flex-column justify-center px2" v-if="showOwnerCol">
       <template v-if="isRFT">
         <p class="h7 m0">Market Cap &clubs;</p>
         <p class="h4 m0 truncate font-mono">0</p>
@@ -23,7 +23,7 @@
         <h5 class="h4 m0 truncate font-mono" style="max-width:4.5em">{{clover.owner}}</h5>
       </template>
     </div>
-
+    <!-- price / RFT share -->
     <div class="col-2 flex flex-column justify-center pl1">
       <template v-if="isRFT">
         <p class="h7 m0">&clubs; / Share</p>
@@ -35,7 +35,7 @@
         <p v-else class="h4 m0 font-mono">---</p>
       </template>
     </div>
-
+    <!-- arrow -->
     <div class="col-1 center flex justify-center pr2">
       <img src="~../assets/icons/arrow-right.svg" width="18" height="18"/>
     </div>
@@ -49,12 +49,16 @@ import BigNumber from 'bignumber.js'
 export default {
   name: 'CloverItem--Row',
   props: {
-    clover: {type: Object, default: () => {}, required: true}
+    clover: {type: Object, default: () => {}, required: true},
+    hideOwner: false
   },
   computed: {
     isRFT () {
       // inCurationMarket
       return this.clover.owner === this.curationMarketAddress
+    },
+    showOwnerCol () {
+      return this.isRFT || !this.hideOwner
     },
     ...mapGetters(['curationMarketAddress'])
   },
