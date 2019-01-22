2a5c4e828937418d5a8eac1dbcc83ba662ac83c9
N Kort <nics@limnerstudio.co.uk>
2019-01-22 12:47:16 +0100

    dedupe bn.js and underscore

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,27 @@
 // var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
-  	plugins: [
-  		// new BundleAnalyzerPlugin(),
-  		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-  	],
+    // optimization: {
+    //   splitChunks: {
+    //     maxSize: 500000
+    //   }
+    // },
+    plugins: [
+      // new BundleAnalyzerPlugin(),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ],
     output: {
       globalObject: 'this'
+    },
+    resolve: {
+      alias: {
+        "bn.js": path.resolve(__dirname, 'node_modules/bn.js'),
+        "underscore": path.resolve(__dirname, 'node_modules/underscore')
+      }
     }
   }
 }
