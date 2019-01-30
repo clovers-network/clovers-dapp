a28de2ccb6beec767e82f25b5c3c6bffb3380193
evvvritt <evv.info@gmail.com>
2019-01-31 00:18:37 +0100

    Users :)

---
+++ src/components/CloverList--Cards.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  ul.list-reset.flex.flex-wrap.m0.overflow-hidden
+    li(v-for="(clover, i) in clovers", :key="clover.board", class="col-6 sm-col-4 md-col-3")
+      router-link(:to="cloverLink(clover)", class="block green border-bottom border-bottom-dotted border-right border-right-dotted", :class="{'xs-border-right-transp': i % 2 === 1, 'sm-border-right-transp': i % 3 === 2, 'md-border-right-transp': i % 4 === 3}")
+        clover-item(:clover="clover")
+</template>
+
+<script>
+import CloverItem from '@/components/CloverItem--Card'
+import { cloverLink } from '@/utils'
+export default {
+  name: 'CloverList--Cards',
+  props: ['clovers'],
+  components: { CloverItem },
+  methods: { cloverLink }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  article.green.relative(v-if="user")
+    header.sticky.top-header-h.left-0.bg-white.z1.border-bottom.md-py2
+      h1.block.m-auto.px2.font-mono.h-header.flex
+        span.block.m-auto {{user.name}}
+    section
+      clover-list-cards(v-if="clovers.length", :clovers="clovers")
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import CloverListCards from '@/components/CloverList--Cards'
+import moment from 'moment'
+export default {
+  name: 'User',
+  props: ['addr'],
+  components: { CloverListCards },
+  computed: {
+    ...mapState(['allUsers', 'allClovers']),
+    user () {
+      if (!this.allUsers.length || !this.addr) return false
+      return this.allUsers.filter(usr => usr.address === this.addr)[0]
+    },
+    clovers () {
+      if (!this.allClovers.length || !this.user) return []
+      return this.user.clovers.map(clvr => this.allClovers.filter(itm => itm && itm.board === clvr)[0])
+    }
+  }
+}
+</script>
+
+<style>
+</style>
