6a1e99601a2bf8e05ff040db4896c082765e0c11
evvvritt <evv.info@gmail.com>
2019-08-17 17:38:06 +0200

    PageTitle > Albums

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-  .mt3.sm-mt4.mxn2
-    more-information.mx2(:btn="false", :parentShow="info")
-      slot(name="info")
+  .mt3.sm-mt4.sm-pt1.mxn2
     .flex.items-center.px1.sm-px2.lh1
       .font-exp(style="font-size:2.25em")
         slot
-      button.pointer.font-reg.rounded-full.bg-lightest-green.lh1.flex.items-center.justify-center.opacity-75.hover-opacity-100(style="width:1.4em; height:1.4em; font-size:10.5px; margin-top:0.1em; padding-bottom:0.05em; margin-left:1.66em", @click="info = !info", title="More Info") ?
+      button.pointer.font-reg.rounded-full.bg-lightest-green.lh1.flex.items-center.justify-center.opacity-75.hover-opacity-100(style="width:1.4em; height:1.4em; font-size:10.5px; margin-top:1em; padding-bottom:0.05em; margin-left:1.66em", @click="info = !info", title="More Info") ?
+    more-information.mx2(:btn="false", :parentShow="info")
+      slot(name="info")
 
 </template>
 
--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
   article.mt2.md-mt4.mx3.md-mx0
-    more-information(title="?") <b>Albums</b> are for grouping clovers together. Users can add any clover to any album but album editors can remove them. You can create albums in your <router-link :to="{name: 'Account'}" class="underline">Dashboard</router-link>, and add to them on any clover's page.
+    header
+      page-title
+        h1 Albums
+        p(slot="info") With <b>Albums</b>, you can group clovers together into a theme. Users can add <i>any</i> clover to <i>any</i> album but Editors can remove them. You can create new albums in your <router-link :to="{name: 'Account'}" class="underline">Dashboard</router-link>, and add to them from any clover's page.
     paged-albums
 </template>
 
 <script>
 import PagedAlbums from '@/components/PagedAlbums'
-import MoreInformation from '@/components/MoreInformation'
+import PageTitle from '@/components/PageTitle'
 export default {
   name: 'Albums',
-  components: { PagedAlbums, MoreInformation }
+  components: { PagedAlbums, PageTitle }
 }
 </script>
