f20da28098d0d9a02ee1d5f8b29fd9c15c330c34
evvvritt <evv.info@gmail.com>
2019-08-19 19:38:58 +0200

    MoreInformation - rmv pointer since can open from parent

--- src/components/MoreInformation.vue
+++ src/components/MoreInformation.vue
@@ -3,7 +3,7 @@
       .absolute.right-0.top-0.mt4.pt2.mr3(v-if="btn")
         button.flex.bg-lightest-green.green.items-center.justify-center(@click="show", style="font-size:0.75em;width:1.5em;height:1.5em;border-radius:100%")
           span.block(style="transform:translateY(-5%)") ?
-      .mb0.pt2.h4.my3.pointer.bg-lightest-green.rounded.p3(v-if="showMore || parentShow")
+      .mb0.pt2.h4.my3.bg-lightest-green.rounded.p3(v-if="showMore || parentShow")
         slot
 </template>
 <script>
