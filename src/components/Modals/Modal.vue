e169ae6e3cf6d184d76b32c70e194422d2cd8e58
evvvritt <evv.info@gmail.com>
2019-08-25 18:07:59 +0200

    Modal - always show X for close (rmv Cancel)

--- src/components/Modals/Modal.vue
+++ src/components/Modals/Modal.vue
@@ -7,8 +7,8 @@
       nav.absolute.z2.top-0.left-0.col-12.h-header.flex.justify-between.items-center.px2
         //- btn: close
         button.p1.pointer(@click="close")
-          span.h5(v-if="cancel") Cancel
-          svg-x(v-else, style="width:1.2rem;height:1.2rem", thickness="1.1")
+          //- span.h5(v-if="cancel") Cancel
+          svg-x(style="width:1.2rem;height:1.2rem", thickness="1.1")
         //- (right-btn)
         slot(name="top-right-btn")
       //- main
