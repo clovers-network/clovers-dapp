fc78bb5cd4b2b6fbe80eaf994ed99a3ff945fdee
evvvritt <evv.info@gmail.com>
2020-04-17 23:45:13 +0200

    Featured - dashed lines

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-  aside.mxn3.md-mt3.py3.px2.md-border.md-shadow.flex.flex-wrap.justify-between.rounded(style="margin-bottom:-1rem", v-if="albums.length")
+  aside.mxn3.md-mt3.py3.px2.md-border-dashed.md-shadow.flex.flex-wrap.justify-between.rounded(style="margin-bottom:-1rem", v-if="albums.length")
     header.col-12.md-col-3.px1.md-px2.pb2
       h3.h2.md-h3.font-exp Spotlight&nbsp;✨
       //- album-list-cards.mt2(v-if="albums.length", :albums="albums")
     router-link.block.my1.col-6.sm-col-4.md-col-3.px1.sm-px2(v-for="(albm, i) in albums", :to="{name: 'Album', params: {id: albm.id}}", :class="{'hidden sm-block': i > 1}")
-      album-item-card.border(:album="albm")
+      album-item-card(:album="albm")
 </template>
 
 <script>
